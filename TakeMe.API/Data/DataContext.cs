045f5a5ea0f60776d8725b233a15678456a8bb73
Egidijus <egidijus.brazaitis@gmail.com>
2018-09-16 17:48:00 +0300

    Initial commit

---
+++ TakeMe.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TakeMe.API.Models;
+
+namespace TakeMe.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Item> Items { get; set;}
+    }
+}
---
+++ TakeMe.API/Models/Item.cs
@@ -0,0 +1,8 @@
+namespace TakeMe.API.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
