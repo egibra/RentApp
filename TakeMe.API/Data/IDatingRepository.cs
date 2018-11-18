c84ccf7951d061a981af9dc7ba4b8c981151bf71
Egidijus <egidijus.brazaitis@gmail.com>
2018-11-18 17:21:11 +0200

    Finished Filtering and sorting

--- TakeMe.API/Data/IDatingRepository.cs
+++ TakeMe.API/Data/IDatingRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using TakeMe.API.Helpers;
 using TakeMe.API.Models;
 
 namespace TakeMe.API.Data
@@ -9,7 +10,7 @@ namespace TakeMe.API.Data
          void Add<T>(T entity) where T: class;
          void Delete<T>(T entity) where T: class;
          Task<bool> SaveAll();
-         Task<IEnumerable<User>> GetUsers();
+         Task<PagedList<User>> GetUsers(UserParams userParams);
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
          Task<Photo> GetMainPhotoForUser(int userId);
